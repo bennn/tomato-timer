c9b0fdc09361af5e43e6743d6ed543e43ad02574
Ben Greenman <benjaminlgreenman@gmail.com>
2017-02-24 15:01:30 -0500

    accumulate & print TODO list (needs docs)

--- main.rkt
+++ main.rkt
@@ -3,8 +3,51 @@
 ;; Sleep for N minutes
 ;;   racket tomato-timer.rkt N
 
+;; =============================================================================
+
+;; make-piper : input-port? output-port? -> thread?
+;; Create a thread to pipe input from `in-port` to `out-port`.
+;; Immediately returns a new thread descriptor.
+(define (make-piper in-port out-port)
+  (thread (λ ()
+    (let loop ()
+      (let ([ln (read-line in-port)])
+        (when (not (eof-object? ln))
+          (displayln ln out-port)
+          (loop)))))))
+
+;; make-sleeper : exact-postive-integer? -> thread?
+;; Create a thread that sleeps for a given number of minutes.
+;; Immediately returns a new thread descriptor.
+(define (make-sleeper minutes)
+  (thread (λ () (sleep (* 60 minutes)))))
+
+;; print-todo-list : input-port -> void?
+;; Read input from a port that describes a TODO list
+(define (print-todo-list in-port)
+  (displayln "TIME UP")
+  (define ln (read-line in-port))
+  (when (not (eof-object? ln))
+    (displayln "postponed items:")
+    (displayln (format-todo-item ln))
+    (for ([ln (in-lines in-port)])
+      (format-todo-item ln)
+      (void))))
+
+;; format-todo-item : string? -> string?
+(define (format-todo-item str)
+  (string-append "- [ ] " str))
+
 (define (tomato-timer minutes)
-  (sleep (* 60 minutes)))
+  (define-values (_in _out) (make-pipe #f 'tomato-in 'tomato-out))
+  (define t (make-piper (current-input-port) _out))
+  (define s (make-sleeper minutes))
+  (thread-wait s)
+  (kill-thread t)
+  (close-output-port _out)
+  (print-todo-list _in)
+  (close-input-port _in)
+  (void))
 
 (module+ main
   (require racket/cmdline)
